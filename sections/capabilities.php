47516fe63d893c861d393587677cf92389f2f78a
早野 龍輝 <hayano@media-confidence.com>
2026-01-22 12:07:55 +0900

    fix: できることセクションを本番に準拠（番号付きレイアウト）

--- sections/capabilities.php
+++ sections/capabilities.php
@@ -1,25 +1,34 @@
 <section class="capabilities">
-		<div class="container">
-			<h3 class="uk-text-center">予約ルームズで<br class="sp">できること</h3>
-		</div>
-			<ul class="capabilities_ul">
-			<li class="capabilities_ul_li">
-				<a href="#sec_management">
-				<h4><span>管理強化で</span><br>利用マナーの改善</h4>
-					<img src="<?php echo get_template_directory_uri(); ?>/img/3-0-12x.webp" width="368" height="193.2" alt="会議室の管理を強化する" >
-				</a>
-			</li>
-			<li class="capabilities_ul_li">
-				<a href="#sec_analytucs">
-				<h4><span>自動分析で</span><br>改善ポイントの特定</h4>
-					<img src="<?php echo get_template_directory_uri(); ?>/img/3-0-2.webp" width="368" height="193.2" alt="会議室を自動分析する" >
-				</a>
-			</li>
-			<li class="capabilities_ul_li">
-				<a href="#top_reserve">
-				<h4><span>選べる予約・入室方法で</span><br>様々なシーンに対応</h4>
-					<img src="<?php echo get_template_directory_uri(); ?>/img/3-0-3.webp" width="368" height="193.2" alt="会議室の予約が方法のバリエーションが豊富" >
-				</a>
-			</li>
-		</ul>
+	<div class="container">
+		<h3 class="uk-text-center">予約ルームズで<br class="sp">できること</h3>
+	</div>
+	<ul class="capabilities_ul container">
+		<li class="capabilities_ul_li">
+			<a href="#sec_management">
+				<div class="capabilities_num"><span class="en">01.</span></div>
+				<h4><span>管理強化で</span>利用マナーの改善</h4>
+				<div class="capabilities_img">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/3-0-12x.webp" width="368" height="193" alt="会議室の管理を強化する">
+				</div>
+			</a>
+		</li>
+		<li class="capabilities_ul_li">
+			<a href="#sec_analytics">
+				<div class="capabilities_num"><span class="en">02.</span></div>
+				<h4><span>自動分析で</span>改善ポイントの特定</h4>
+				<div class="capabilities_img">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/3-0-2.webp" width="368" height="193" alt="会議室を自動分析する">
+				</div>
+			</a>
+		</li>
+		<li class="capabilities_ul_li">
+			<a href="#top_reserve">
+				<div class="capabilities_num"><span class="en">03.</span></div>
+				<h4><span>選べる予約・入室方法で</span>様々なシーンに対応</h4>
+				<div class="capabilities_img capabilities_img_grid">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/3-0-3.webp" width="368" height="193" alt="会議室の予約方法のバリエーションが豊富">
+				</div>
+			</a>
+		</li>
+	</ul>
 </section>
